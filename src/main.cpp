a5805da615492df9cf5104a88038552f693556e5
nitronoid <jackdiver@hotmail.co.uk>
2019-01-23 00:18:11 +0000

    Added comments, constraint projection code is much cleaner, and more flexible (would like to use a multi-span though). Need to factor it out into other files, along with the now fully working willmore flow.

--- src/main.cpp
+++ src/main.cpp
@@ -16,87 +16,120 @@ using namespace Eigen;
 using namespace flo;
 
 
-template <typename T, typename F>
-MatrixXd build_constraint_basis(const T& i_vectors, F&& i_inner_product)
+template <typename F>
+std::vector<double> orthonormalize(
+    const gsl::span<const double> i_vectors, 
+    const uint i_num_vectors, 
+    F&& i_inner_product)
 {
   // Normalize is defined using a self inner product
   auto normalize = [&](const auto& x) {
-    return x.array() / std::sqrt(i_inner_product(x, x)(0, 0));
+    return x.array() / std::sqrt(i_inner_product(x, x));
   };
-  const auto vlen = i_vectors.size();
-  std::cout<<vlen<<'\n';
-  const auto n_vec = sizeof(i_vectors[0]) / sizeof(double);
+  const auto vlen = i_vectors.size() / i_num_vectors;
 
   // Map our input vectors as a matrix
-  auto matrix = array_to_matrix(gsl::make_span(i_vectors));
+  Map<const MatrixXd> v(i_vectors.data(), vlen, i_num_vectors); 
   //std::cout<<matrix<<'\n';
 
   // Declare and allocate space for our final basis matrix
-  MatrixXd basis(vlen, n_vec);
+  std::vector<double> basis(vlen * i_num_vectors);
+  Map<MatrixXd> u(basis.data(), vlen, i_num_vectors); 
 
-  // The first vector is filled with normalized ones
-  basis.col(0) = normalize(matrix.col(0));
+  // The first u0 is v0 normalized
+  u.col(0) = normalize(v.col(0));
   // Gramm Schmit process
-  for (uint i = 1u; i < n_vec; ++i)
+  for (uint i = 1u; i < i_num_vectors; ++i)
   {
-    basis.col(i) =
-      matrix.col(i) -
-      i_inner_product(matrix.col(i), basis.col(0)) * basis.col(0);
+    u.col(i) = v.col(i) - i_inner_product(v.col(i), u.col(0)) * u.col(0);
     for (uint k = 1u; k < i; ++k)
     {
-      basis.col(i) -= 
-        i_inner_product(basis.col(i), basis.col(k)) * basis.col(k);
+      u.col(i) -= i_inner_product(u.col(i), u.col(k)) * u.col(k);
     }
-    basis.col(i) = normalize(basis.col(i).eval());
+    u.col(i) = normalize(u.col(i).eval());
   }
 
   return basis;
 }
 
 template <typename I>
-void project_constraints(gsl::span<double> io_curvature,
-                         const float i_step,
-                         const MatrixXd& i_basis,
-                         I&& i_inner_product)
+std::vector<double> project_constraint_basis(
+      const gsl::span<const double> i_curvature,
+      const gsl::span<const double> i_basis, 
+      const uint i_num_constraints, 
+      I&& i_inner_product)
 {
-  const auto vlen = io_curvature.size();
-  const auto n_vecs = i_basis.cols();
-  auto kappa_dot = array_to_matrix(io_curvature);
+  const auto vlen = i_curvature.size();
+  std::vector<double> projected(i_curvature.begin(), i_curvature.end());
+  auto kappa_dot = array_to_matrix(gsl::make_span(projected));
+  Map<const MatrixXd> u(i_basis.data(), vlen, i_num_constraints); 
 
-  VectorXd accum(vlen);
-  accum.setZero();
-  for (uint i = 0u; i < n_vecs; ++i)
+  // Subtract the projected curvature from the un-projected
+  for (uint i = 0u; i < i_num_constraints; ++i)
   {
-    accum += i_inner_product(kappa_dot, i_basis.col(i))(0, 0) * i_basis.col(i);
+    kappa_dot -= i_inner_product(kappa_dot, u.col(i)) * u.col(i);
   }
-  kappa_dot = i_step * (kappa_dot - accum);
+
+  return projected;
+}
+
+void forward_euler(gsl::span<double> i_x,
+                   const gsl::span<const double> i_dx,
+                   const double i_t)
+{
+  std::transform(i_x.begin(), i_x.end(), i_dx.begin(), i_x.begin(),
+                 [i_t](auto x, auto dx)
+                 {
+                   return x + dx * i_t;
+                 });
 }
 
 int main()
 {
   auto surf = flo::load_mesh("foo.obj");
 
-  for (int iter = 0; iter < 6; ++iter)
+  for (int iter = 0; iter < 10; ++iter)
   {
+  // Calculate smooth vertex normals
   auto normals = flo::vertex_normals(surf.vertices, surf.faces);
 
   // Calculate the cotangent laplacian for our mesh
   auto L = flo::cotangent_laplacian(surf.vertices, surf.faces);
+  // Calculate the vertex masses for our mesh 
   auto mass = vertex_mass(surf.vertices, surf.faces);
+  // Map the masses to a vector (to use as a diagonal-matrix)
   auto M = array_to_matrix(gsl::make_span(mass));
 
-  std::vector<Vector4d> constraints; constraints.reserve(normals.size());
+  const uint n_verts = surf.n_vertices();
+  const uint n_constraints = 4;
+  // Build our constraints {1, N.x, N.y, N.z}
+  std::vector<double> constraints(normals.size() * n_constraints);
   for (int i = 0; i < normals.size(); ++i)
-    constraints.emplace_back(1., normals[i].x(), normals[i].y(), normals[i].z()); 
+  {
+    constraints[i + n_verts * 0] = 1.0;
+    constraints[i + n_verts * 1] = normals[i].x();
+    constraints[i + n_verts * 2] = normals[i].y();
+    constraints[i + n_verts * 3] = normals[i].z();
+  }
 
-  const auto ip = [&](const auto& x, const auto& y) {
-    return x.transpose() * M.asDiagonal() * y;
+  // Declare an immersed inner-product using the mass matrix
+  const auto ip = [&M](const auto& x, const auto& y) -> double {
+    auto single_mat = (x.transpose() * M.asDiagonal() * y).eval();
+    return single_mat(0,0);
   };
-  auto basis = build_constraint_basis(normals, ip);
+  // Build a constraint basis using the Gram–Schmidt process
+  auto basis = orthonormalize(constraints, n_constraints, ip);
 
+  // Calculate the signed mean curvature based on our vertex normals
   auto mc = signed_mean_curvature(surf.vertices, L, mass, normals);
-  project_constraints(mc, 0.95, basis, ip);
+  // Apply our flow direction to the the mean curvature half density
+  std::transform(mc.begin(), mc.end(), mc.begin(), [](auto x) { return -x; });
+  // project the constraints on to our mean curvature
+  auto projected = project_constraint_basis(mc, basis, n_constraints, ip);
+  // take a time step
+  forward_euler(mc, projected, 0.95);
 
+  // spin transform using our change in mean curvature half-density
   surf.vertices = flo::spin_xform(surf.vertices, surf.faces, mc, L);
   }
 
