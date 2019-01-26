0e923072b3a9699b39a13e69d64e43a88f4a5c64
nitronoid <jackdiver@hotmail.co.uk>
2019-01-26 17:40:38 +0000

    Removed spans from kernel, too many oversights in the library so we'll use the traditional approach. Updated prefixes to identify device memory. Final implementation not yet finished.

--- flo/src/flo/device/vertex_mass.cu
+++ flo/src/flo/device/vertex_mass.cu
@@ -1,17 +1,5 @@
 #include "flo/device/vertex_mass.cuh"
 #include "flo/host/area.hpp"
-//#define TMPGNUC __GNUC__
-//#define __STRINGIFY(TEXT) #TEXT
-//#define __WARNING(TEXT) __STRINGIFY(GCC warning TEXT)
-//#define WARNING(VALUE) __WARNING(__STRINGIFY(N = VALUE))
-//_Pragma (WARNING(TMPGNUC))
-//#define __GNUC__ 6
-//#include <cuda.h>
-//#include <curand.h>
-//#include <cuda_runtime.h>
-//#include <cuda_runtime_api.h>
-//#undef TMPGNUC
-//#define __GNUC__ TMPGNUC 
 
 using namespace Eigen;
 
@@ -20,26 +8,27 @@ FLO_DEVICE_NAMESPACE_BEGIN
 namespace
 {
 __global__ void vertex_mass_impl(
-    const gsl::span<const int> i_vertex_face_adjacency,
-    const gsl::span<const int> i_vertex_face_valence,
-    const gsl::span<const int> i_vertex_offset,
-    const gsl::span<const double> i_face_area,
-    gsl::span<double> io_mass)
+    const int* di_vertex_face_adjacency,
+    const int* di_vertex_face_valence,
+    const int* di_vertex_offset,
+    const double* di_face_area,
+    double* dio_mass,
+    const uint i_n_verts)
 {
   size_t tid = threadIdx.x + blockIdx.x * blockDim.x;
   // one thread per mass
-  if (tid >= io_mass.size()) return;
+  if (tid >= i_n_verts) return;
 
-  const int valence = i_vertex_face_valence[tid];
-  const int offset = i_vertex_offset[tid];
+  const int valence = di_vertex_face_valence[tid];
+  const int offset = di_vertex_offset[tid];
 
   double total_area = 0.;
   for (int i = 0; i < valence; ++i)
   {
-    total_area += i_face_area[i_vertex_face_adjacency[offset + i]];
+    total_area += di_face_area[di_vertex_face_adjacency[offset + i]];
   }
   constexpr auto third = 1.f / 3.f;
-  io_mass[tid] = total_area * third;
+  dio_mass[tid] = total_area * third;
 }
 }
 
@@ -49,6 +38,8 @@ std::vector<double> vertex_mass(
 {
   std::vector<double> mass(i_vertices.size());
   auto face_area = host::area(i_vertices, i_faces);
+  std::vector<int> vertex_adjacency;
+  std::vector<int> vertex_offset;
 
   // For every face
   for (uint i = 0; i < i_faces.size(); ++i)
