0dc2fc0723e51cf5d59e16a5a416aa3360c81e54
nitronoid <jackdiver@hotmail.co.uk>
2019-02-06 01:45:22 +0000

    Finished first version of the laplace beltrami triplet kernel, need to test it now.

--- flo/src/flo/device/cotangent_laplacian.cu
+++ flo/src/flo/device/cotangent_laplacian.cu
@@ -4,12 +4,29 @@ FLO_DEVICE_NAMESPACE_BEGIN
 
 namespace
 {
+__device__ uint unique_thread_idx1()
+{
+  return
+    // Global block index
+    (blockIdx.x) *
+    // Number of threads in a block
+    (blockDim.x) + 
+    // thread index in block
+    (threadIdx.x);
+}
 
-__device__ uint unique_thread_idx(
-    const dim3&  k_grid_dim, 
-    const uint3& k_block_idx, 
-    const dim3&  k_block_dim, 
-    const uint3& k_thread_idx)
+__device__ uint unique_thread_idx2()
+{
+  return
+    // Global block index
+    (blockIdx.x + blockIdx.y * gridDim.x) *
+    // Number of threads in a block
+    (blockDim.x * blockDim.y) + 
+    // thread index in block
+    (threadIdx.x + threadIdx.y * blockDim.x);
+}
+
+__device__ uint unique_thread_idx3()
 {
   return
     // Global block index
@@ -20,56 +37,107 @@ __device__ uint unique_thread_idx(
     (threadIdx.x + threadIdx.y * blockDim.x + threadIdx.z * blockDim.x * blockDim.y);
 }
 
-// Pairs of face vert indices for triangle edges,
-// corresponding to our matrix entries
-__constant__ uchar2 dk_vertex_pairs[12];
+__device__ uint8_t cycle(uint8_t i_x)
+{
+  /************************
+     mapping is as follows
+     0 -> 2
+     1 -> 0
+     2 -> 1
+  ************************/
+    uint8_t c = i_x + 0xFC;
+    return  __ffs(c) - 1;
+}
+
+__device__ uchar3 edge_loop(uint8_t i_e)
+{
+  uchar3 loop;
+  loop.x = i_e;
+  loop.z = cycle(loop.x);
+  loop.y = cycle(loop.z);
+  return loop;
+}
 
 // block dim should be M*3*4, where M is some number of faces,
 // we have three edges per triangle face, and write four values per edge
 __global__ void d_build_triplets(
     const thrust::device_ptr<const int3> di_faces,
-    const thrust::device_ptr<const real> di_C,
-    const uint i_nentries,
+    const thrust::device_ptr<const real3> di_vertices,
+    const thrust::device_ptr<const real> di_face_area,
+    const uint i_nfaces,
     thrust::device_ptr<int> do_I,
     thrust::device_ptr<int> do_J,
     thrust::device_ptr<real> do_V)
 {
-  const uint tid = unique_thread_idx(gridDim, blockIdx, blockDim, threadIdx);
+  // Declare one shared memory block
+  extern __shared__ real shared_memory[];
+  // Create pointers into the block dividing it for the different uses
+  real* area = shared_memory;
+  // There are nfaces number of areas so we offset
+  real* cot_alpha = &shared_memory[i_nfaces];
+  // There are nfaces number of areas and 3*nfaces number of alphas, offset
+  uchar2* vertex_pairs = (uchar2*)&shared_memory[i_nfaces*4];
 
-  // Check we're not out of range
-  if (tid >= i_nentries) return;
+  // calculated from the x index only
+  const uint f_idx = unique_thread_idx1();
 
-  const uint f_idx = threadIdx.x + blockIdx.x * blockDim.x;
+  // Write the face area to shared memory to reduce global memory reads
+  if (f_idx < i_nfaces)
+  {
+    area[f_idx] = di_face_area[f_idx];
+  }
+  __syncthreads();
 
-  // Get the relavent face, calculated from the x index only
+  // calculated from the x and y indices
+  const uint e_idx = unique_thread_idx2();
+
+  // Get the relevant face
   const int3 f = di_faces[f_idx];
 
-  // Get the source and dest vertex indices for this edge
-  const uchar2 vpair = dk_vertex_pairs[threadIdx.y * blockDim.z + threadIdx.z];
+  // Save the cotangent value into shared memory as multiple threads will,
+  // write it into the final matrix
+  if (e_idx < i_nfaces * blockDim.y)
+  {
+    // Get the ordered other vertices
+    const uchar3 loop = edge_loop(threadIdx.y);
+    // Store vertex pairs for later
+    vertex_pairs[e_idx + 0].x = loop.y;
+    vertex_pairs[e_idx + 0].y = loop.z;
+    vertex_pairs[e_idx + 1].x = loop.z;
+    vertex_pairs[e_idx + 1].y = loop.y;
+    vertex_pairs[e_idx + 2].x = loop.y;
+    vertex_pairs[e_idx + 2].y = loop.y;
+    vertex_pairs[e_idx + 3].x = loop.z;
+    vertex_pairs[e_idx + 3].y = loop.z;
+    // Get positions of vertices
+    const real3 v0 = di_vertices[(&f.x)[loop.x]];
+    const real3 v1 = di_vertices[(&f.x)[loop.y]];
+    const real3 v2 = di_vertices[(&f.x)[loop.z]];
 
-  // Will replace this with prettier functions later
-  do_I[tid] = *((&f.x) + vpair.x);
-  do_J[tid] = *((&f.x) + vpair.y);
-  do_V[tid] = di_C[f_idx*3 + threadIdx.y];
-}
+    const real3 e0 = v2 - v1;
+    const real3 e1 = v0 - v2;
+    const real3 e2 = v1 - v0;
+    
+    // Value is sum of opposing squared lengths minus this squared length,
+    // divided by 4 times the area
+    cot_alpha[e_idx] =
+      (dot(e1,e1) + dot(e2,e2) - dot(e0,e0)) / (area[f_idx] * 8.f);
+  }
+  __syncthreads();
 
-__global__ void d_calculate_cotangent_values(
-    const thrust::device_ptr<const int3> di_faces,
-    const thrust::device_ptr<const real3> di_vertices,
-    const thrust::device_ptr<const real> di_face_area,
-    const uint i_nfaces,
-    thrust::device_ptr<real> do_C)
-{
-  const uint tid = unique_thread_idx(gridDim, blockIdx, blockDim, threadIdx);
+  // calculated from the x, y and z indices
+  const uint tid = unique_thread_idx3();
 
   // Check we're not out of range
-  if (tid >= i_nfaces * 3) return;
+  if (tid >= i_nfaces * blockDim.y * blockDim.z) return;
 
-  // reuse const memory
-  // Get the source and dest vertex indices for this edge
-  const uchar2 vpair = dk_vertex_pairs[threadIdx.y];
+  // Get the source and dest vertex indices for this entry 
+  const uchar2 vpair = vertex_pairs[tid];
 
-  do_C[tid] = /*calc here*/0;
+  // Will replace this with prettier functions later
+  do_I[tid] = (&f.x)[vpair.x];
+  do_J[tid] = (&f.x)[vpair.y];
+  do_V[tid] = cot_alpha[e_idx];
 }
 }
 
@@ -77,7 +145,6 @@ __global__ void d_calculate_cotangent_values(
 FLO_API cusp::coo_matrix<int, real, cusp::device_memory> cotangent_laplacian(
     const thrust::device_ptr<const real3> di_vertices,
     const thrust::device_ptr<const int3> di_faces,
-    const thrust::device_ptr<const real> di_face_area,
     const int i_nverts,
     const int i_nfaces,
     const int i_total_valence)
@@ -89,7 +156,6 @@ FLO_API cusp::coo_matrix<int, real, cusp::device_memory> cotangent_laplacian(
   thrust::device_vector<int> I(ntriplets);
   thrust::device_vector<int> J(ntriplets);
   thrust::device_vector<real> V(ntriplets);
-  thrust::device_vector<real3> C(ntriplets);
 
 
   return d_L;
