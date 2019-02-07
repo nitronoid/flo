3c93bae0d15183a2de51ddb6ba6ab298ec177faf
nitronoid <jackdiver@hotmail.co.uk>
2019-02-07 00:33:08 +0000

    Fixed the laplace beltrami device function, need to test it on other meshes. Tidied up the code a little, now cache vertex positions in shared memory. Need to investigate how bad the bank conflicts are as I haven't paid much attention to them yet.

--- flo/src/flo/device/cotangent_laplacian.cu
+++ flo/src/flo/device/cotangent_laplacian.cu
@@ -7,7 +7,14 @@ FLO_DEVICE_NAMESPACE_BEGIN
 
 namespace
 {
-__device__ uint unique_thread_idx1()
+template <typename T>
+__device__ __forceinline__ auto 
+nth_element(const T& i_tuple, uint8_t i_index) -> decltype(i_tuple.x)
+{
+  return (&i_tuple.x)[i_index];
+}
+
+__device__ __forceinline__ uint unique_thread_idx1()
 {
   return
     // Global block index
@@ -18,7 +25,7 @@ __device__ uint unique_thread_idx1()
     (threadIdx.x);
 }
 
-__device__ uint unique_thread_idx2()
+__device__ __forceinline__ uint unique_thread_idx2()
 {
   return
     // Global block index
@@ -29,7 +36,7 @@ __device__ uint unique_thread_idx2()
     (threadIdx.x + threadIdx.y * blockDim.x);
 }
 
-__device__ uint unique_thread_idx3()
+__device__ __forceinline__ uint unique_thread_idx3()
 {
   return
     // Global block index
@@ -40,7 +47,7 @@ __device__ uint unique_thread_idx3()
     (threadIdx.x + threadIdx.y * blockDim.x + threadIdx.z * blockDim.x * blockDim.y);
 }
 
-__device__ uint8_t cycle(uint8_t i_x)
+__device__ __forceinline__ uint8_t cycle(uint8_t i_x)
 {
   /************************
      mapping is as follows
@@ -52,14 +59,13 @@ __device__ uint8_t cycle(uint8_t i_x)
     return  __ffs(c) - 1;
 }
 
-__device__ uchar3 edge_loop(uint8_t i_e)
+__device__ __forceinline__ uchar3 edge_loop(uint8_t i_e)
 {
   uchar3 loop;
   loop.x = i_e;
   loop.z = cycle(loop.x);
   loop.y = cycle(loop.z);
   return loop;
-}
 }
 
 // block dim should be 4*3*#F, where #F is some number of faces,
@@ -79,14 +85,17 @@ __global__ void d_build_triplets(
   real* area = shared_memory;
   // There are nfaces number of areas so we offset
   real* cot_alpha = area + blockDim.x;
+  // There are is a cot value for each corner of the face so we offset
+  real3* points = (real3*)(cot_alpha + blockDim.x*3);
 
-  // IMPORTANT, we rotate the thread id's
-  const uint tid =
-    // Global block index
-    (blockIdx.x + blockIdx.y * gridDim.x + blockIdx.z * gridDim.x * gridDim.y) *
-    // Number of threads in a block
-    (blockDim.x * blockDim.y * blockDim.z) + 
-    // thread index in block
+  // Block index
+  const uint block_index =
+    blockIdx.x + blockIdx.y * gridDim.x + blockIdx.z * gridDim.x * gridDim.y;
+  // Number of threads in a block
+  const uint n_threads_in_block = blockDim.x * blockDim.y * blockDim.z;
+
+  const uint tid = block_index * n_threads_in_block +
+    // thread index in block. IMPORTANT, we rotate the thread id's
     (threadIdx.y + threadIdx.z * blockDim.y + threadIdx.x * blockDim.y * blockDim.z);
 
   // Check we're not out of range
@@ -94,37 +103,39 @@ __global__ void d_build_triplets(
 
   // calculated from the x index only
   const uint f_idx = blockIdx.x * blockDim.x + threadIdx.x;
-
-  // Write the face area to shared memory to reduce global memory reads
-  if (f_idx < i_nfaces && !threadIdx.y && !threadIdx.z)
-  {
-    area[threadIdx.x] = di_face_area[f_idx];
-  }
-  __syncthreads();
-
   // Get the relevant face
   const int3 f = di_faces[f_idx];
 
+  // Write the face area, and vertex positions in to shared memory,
+  // we do this to reduce global memory reads
+  if (f_idx < i_nfaces && !threadIdx.y && !threadIdx.z)
+  {
+    area[threadIdx.x] = di_face_area[f_idx];
+    points[threadIdx.x*3 + 0] = di_vertices[f.x];
+    points[threadIdx.x*3 + 1] = di_vertices[f.y];
+    points[threadIdx.x*3 + 2] = di_vertices[f.z];
+  }
+  __syncthreads();
+
+
   const uint e_idx = 
-    // Global block index
-    (blockIdx.x + blockIdx.y * gridDim.x + blockIdx.z * gridDim.x * gridDim.y) *
-    // Number of edges in a block
-    (blockDim.x * blockDim.z) + 
+    // Global block index multiplied by number of edges in a block
+    block_index * (blockDim.x * blockDim.z) + 
     // Edge id in this block
     (threadIdx.z + threadIdx.x * blockDim.z);
 
   // Save the cotangent value into shared memory as multiple threads will,
   // write it into the final matrix
+  // Get the ordered other vertices
+  const uchar3 loop = edge_loop(threadIdx.z);
   if (e_idx < i_nfaces * blockDim.z && !threadIdx.y)
   {
-    // Get the ordered other vertices
-    const uchar3 loop = edge_loop(threadIdx.z);
-
     // Get positions of vertices
-    const real3 v0 = di_vertices[(&f.x)[loop.x]];
-    const real3 v1 = di_vertices[(&f.x)[loop.y]];
-    const real3 v2 = di_vertices[(&f.x)[loop.z]];
+    const real3 v0 = points[threadIdx.x*3 + loop.x];
+    const real3 v1 = points[threadIdx.x*3 + loop.y];
+    const real3 v2 = points[threadIdx.x*3 + loop.z];
 
+    // Get the edge vectors for the face
     const real3 e0 = v2 - v1;
     const real3 e1 = v0 - v2;
     const real3 e2 = v1 - v0;
@@ -136,15 +147,18 @@ __global__ void d_build_triplets(
   }
   __syncthreads();
 
-  // Will replace this with prettier functions later
-  uint8_t source =  (threadIdx.y        & 1u) + 1u;
-  uint8_t dest   = ((threadIdx.y >> 1u) & 1u) + 1u;
-  int sign = (source == dest)*2 - 1;
-  do_I[tid] = (&f.x)[source];
-  do_J[tid] = (&f.x)[dest];
+  // Get the vertex index pair to write our value to 
+  uint8_t source = nth_element(loop,  (threadIdx.y        & 1u) + 1u);
+  uint8_t dest   = nth_element(loop, ((threadIdx.y >> 1u) & 1u) + 1u);
+  // If the pair are non identical, our entry should be negative
+  int8_t sign = (source == dest)*2 - 1;
+  // Write the row and column indices
+  do_I[tid] = nth_element(f, source);
+  do_J[tid] = nth_element(f, dest);
+  // Write the signed value
   do_V[tid] = cot_alpha[e_idx] * sign;
 }
-//}
+}
 
 
 FLO_API cusp::coo_matrix<int, real, cusp::device_memory> cotangent_laplacian(
@@ -169,7 +183,7 @@ FLO_API cusp::coo_matrix<int, real, cusp::device_memory> cotangent_laplacian(
   block_dim.z = 3;
   size_t nthreads_per_block = block_dim.x * block_dim.y * block_dim.z;
   size_t nblocks = ntriplets / nthreads_per_block + 1;
-  size_t shared_memory_size = sizeof(flo::real)*block_dim.x*4;
+  size_t shared_memory_size = sizeof(flo::real)*block_dim.x*7;
 
 
   d_build_triplets<<<nblocks, block_dim, shared_memory_size>>>(
