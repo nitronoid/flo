f951930f96cb2ffcdbb1e6a2b6b082b2683a1fda
nitronoid <jackdiver@hotmail.co.uk>
2019-02-08 23:31:47 +0000

    The cotangenet laplacian now computes more in parallel and makes better use of shared memory. Might investigate placing face indices in shared memory, or passing them as pure integers and reading through the edge index, but before that I need to start profiling and add more tests. Need to check the impact of matrix construction from the tuples, evaluate better sorting, maybe using gathers.

--- flo/src/flo/device/cotangent_laplacian.cu
+++ flo/src/flo/device/cotangent_laplacian.cu
@@ -2,14 +2,13 @@
 #include <thrust/sort.h>
 #include <thrust/reduce.h>
 
-
 FLO_DEVICE_NAMESPACE_BEGIN
 
 namespace
 {
 template <typename T>
-__device__ __forceinline__ auto 
-nth_element(const T& i_tuple, uint8_t i_index) -> decltype(i_tuple.x)
+__device__ __forceinline__ auto nth_element(const T& i_tuple, uint8_t i_index)
+  -> decltype(i_tuple.x)
 {
   return (&i_tuple.x)[i_index];
 }
@@ -19,8 +18,8 @@ __device__ __forceinline__ uint unique_thread_idx1()
   return
     // Global block index
     (blockIdx.x) *
-    // Number of threads in a block
-    (blockDim.x) + 
+      // Number of threads in a block
+      (blockDim.x) +
     // thread index in block
     (threadIdx.x);
 }
@@ -30,8 +29,8 @@ __device__ __forceinline__ uint unique_thread_idx2()
   return
     // Global block index
     (blockIdx.x + blockIdx.y * gridDim.x) *
-    // Number of threads in a block
-    (blockDim.x * blockDim.y) + 
+      // Number of threads in a block
+      (blockDim.x * blockDim.y) +
     // thread index in block
     (threadIdx.x + threadIdx.y * blockDim.x);
 }
@@ -41,10 +40,11 @@ __device__ __forceinline__ uint unique_thread_idx3()
   return
     // Global block index
     (blockIdx.x + blockIdx.y * gridDim.x + blockIdx.z * gridDim.x * gridDim.y) *
-    // Number of threads in a block
-    (blockDim.x * blockDim.y * blockDim.z) + 
+      // Number of threads in a block
+      (blockDim.x * blockDim.y * blockDim.z) +
     // thread index in block
-    (threadIdx.x + threadIdx.y * blockDim.x + threadIdx.z * blockDim.x * blockDim.y);
+    (threadIdx.x + threadIdx.y * blockDim.x +
+     threadIdx.z * blockDim.x * blockDim.y);
 }
 
 __device__ __forceinline__ uint8_t cycle(uint8_t i_x)
@@ -55,12 +55,18 @@ __device__ __forceinline__ uint8_t cycle(uint8_t i_x)
      1 -> 0
      2 -> 1
   ************************/
-    uint8_t c = i_x + 0xFC;
-    return  __ffs(c) - 1;
+  uint8_t c = i_x + 0xFC;
+  return __ffs(c) - 1;
 }
 
 __device__ __forceinline__ uchar3 edge_loop(uint8_t i_e)
 {
+  /************************
+    e.g. input == 1
+     x -> 1
+     y -> 2
+     z -> 0
+  ************************/
   uchar3 loop;
   loop.x = i_e;
   loop.z = cycle(loop.x);
@@ -68,25 +74,25 @@ __device__ __forceinline__ uchar3 edge_loop(uint8_t i_e)
   return loop;
 }
 
-// block dim should be 4*3*#F, where #F is some number of faces,
+// block dim should be #F*4*3, where #F is some number of faces,
 // we have three edges per triangle face, and write four values per edge
-__global__ void d_build_triplets(
-    const thrust::device_ptr<const real3> di_vertices,
-    const thrust::device_ptr<const int3> di_faces,
-    const thrust::device_ptr<const real> di_face_area,
-    const uint i_nfaces,
-    thrust::device_ptr<int> do_I,
-    thrust::device_ptr<int> do_J,
-    thrust::device_ptr<real> do_V)
+__global__ void
+d_build_triplets(const thrust::device_ptr<const real3> di_vertices,
+                 const thrust::device_ptr<const int3> di_faces,
+                 const thrust::device_ptr<const real> di_face_area,
+                 const uint i_nfaces,
+                 thrust::device_ptr<int> do_I,
+                 thrust::device_ptr<int> do_J,
+                 thrust::device_ptr<real> do_V)
 {
   // Declare one shared memory block
   extern __shared__ real shared_memory[];
   // Create pointers into the block dividing it for the different uses
-  real* area = shared_memory;
-  // There are nfaces number of areas so we offset
-  real* cot_alpha = area + blockDim.x;
-  // There are is a cot value for each corner of the face so we offset
-  real3* points = (real3*)(cot_alpha + blockDim.x*3);
+  real* cached_value = shared_memory;
+  // There are is a cached value for each corner of the face so we offset
+  real3* points = (real3*)(cached_value + blockDim.x * 3);
+  // There are nfaces *3 vertex values (duplicated for each face vertex)
+  real* edge_norm2 = (real*)(points + blockDim.x * 3);
 
   // Block index
   const uint block_index =
@@ -94,80 +100,87 @@ __global__ void d_build_triplets(
   // Number of threads in a block
   const uint n_threads_in_block = blockDim.x * blockDim.y * blockDim.z;
 
-  const uint tid = block_index * n_threads_in_block +
+  const uint tid =
+    block_index * n_threads_in_block +
     // thread index in block. IMPORTANT, we rotate the thread id's
-    (threadIdx.y + threadIdx.z * blockDim.y + threadIdx.x * blockDim.y * blockDim.z);
+    (threadIdx.y + threadIdx.z * blockDim.y +
+     threadIdx.x * blockDim.y * blockDim.z);
 
   // Check we're not out of range
-  if (tid >= i_nfaces * blockDim.y * blockDim.z) return;
+  if (tid >= i_nfaces * blockDim.y * blockDim.z)
+    return;
 
   // calculated from the x index only
   const uint f_idx = blockIdx.x * blockDim.x + threadIdx.x;
   // Get the relevant face
   const int3 f = di_faces[f_idx];
 
-  // Write the face area, and vertex positions in to shared memory,
-  // we do this to reduce global memory reads
-  if (f_idx < i_nfaces && !threadIdx.y && !threadIdx.z)
-  {
-    area[threadIdx.x] = di_face_area[f_idx];
-    points[threadIdx.x*3 + 0] = di_vertices[f.x];
-    points[threadIdx.x*3 + 1] = di_vertices[f.y];
-    points[threadIdx.x*3 + 2] = di_vertices[f.z];
-  }
-  __syncthreads();
-
-
-  const uint e_idx = 
+  const uint global_edge_idx =
     // Global block index multiplied by number of edges in a block
-    block_index * (blockDim.x * blockDim.z) + 
+    block_index * (blockDim.x * blockDim.z) +
     // Edge id in this block
     (threadIdx.z + threadIdx.x * blockDim.z);
 
-  // Save the cotangent value into shared memory as multiple threads will,
-  // write it into the final matrix
-  // Get the ordered other vertices
+  // Get the vertex order
   const uchar3 loop = edge_loop(threadIdx.z);
-  if (e_idx < i_nfaces * blockDim.z && !threadIdx.y)
-  {
-    // Get positions of vertices
-    const real3 v0 = points[threadIdx.x*3 + loop.x];
-    const real3 v1 = points[threadIdx.x*3 + loop.y];
-    const real3 v2 = points[threadIdx.x*3 + loop.z];
+  // Compute local edge indices rotated by the vertex order
+  const int e_idx0 = threadIdx.x * 3 + loop.x;
+  const int e_idx1 = threadIdx.x * 3 + loop.y;
+  const int e_idx2 = threadIdx.x * 3 + loop.z;
 
-    // Get the edge vectors for the face
-    const real3 e0 = v2 - v1;
-    const real3 e1 = v0 - v2;
-    const real3 e2 = v1 - v0;
-    
-    // Value is sum of opposing squared lengths minus this squared length,
-    // divided by 4 times the area
-    cot_alpha[e_idx] =
-      (dot(e1,e1) + dot(e2,e2) - dot(e0,e0)) / (area[threadIdx.x] * 8.f);
+  if (f_idx < i_nfaces && !threadIdx.y && !threadIdx.z)
+  {
+    // Write the face area, we do this to reduce global memory reads
+    real scaled_area = di_face_area[f_idx] * 8.f;
+    // Duplicate for each corner of the face to reduce bank conflicts
+    cached_value[e_idx0] = cached_value[e_idx1] = cached_value[e_idx2] =
+      scaled_area;
   }
   __syncthreads();
 
-  // Get the vertex index pair to write our value to 
-  uint8_t source = nth_element(loop,  (threadIdx.y        & 1u) + 1u);
-  uint8_t dest   = nth_element(loop, ((threadIdx.y >> 1u) & 1u) + 1u);
-  // If the pair are non identical, our entry should be negative
-  int8_t sign = (source == dest)*2 - 1;
+  // Run a thread for each face-edge
+  bool edge_thread = global_edge_idx < i_nfaces * blockDim.z && !threadIdx.y;
+  if (edge_thread)
+  {
+    // Write the vertex positions into shared memory
+    points[e_idx0] = di_vertices[nth_element(f, loop.x)];
+  }
+  __syncthreads();
+  if (edge_thread)
+  {
+    // Compute squared length of edges and write to shared memory
+    const real3 e = points[e_idx2] - points[e_idx1];
+    edge_norm2[e_idx0] = dot(e, e);
+  }
+  __syncthreads();
+  if (edge_thread)
+  {
+    real area = cached_value[e_idx0];
+    // Save the cotangent value into shared memory as multiple threads will,
+    // write it into the final matrix
+    cached_value[e_idx0] =
+      (edge_norm2[e_idx1] + edge_norm2[e_idx2] - edge_norm2[e_idx0]) / area;
+  }
+  __syncthreads();
+
+  // Get the vertex index pair to write our value to
+  uint8_t source = nth_element(loop, (threadIdx.y & 1u) + 1u);
+  uint8_t dest = nth_element(loop, ((threadIdx.y >> 1u) & 1u) + 1u);
   // Write the row and column indices
   do_I[tid] = nth_element(f, source);
   do_J[tid] = nth_element(f, dest);
-  // Write the signed value
-  do_V[tid] = cot_alpha[e_idx] * sign;
+  // If the vertex pair are non identical, our entry should be negative
+  do_V[tid] = cached_value[e_idx0] * ((source == dest) * 2 - 1);
 }
-}
+}  // namespace
 
-
-FLO_API cusp::coo_matrix<int, real, cusp::device_memory> cotangent_laplacian(
-    const thrust::device_ptr<const real3> di_vertices,
-    const thrust::device_ptr<const int3> di_faces,
-    const thrust::device_ptr<const real> di_face_area,
-    const int i_nverts,
-    const int i_nfaces,
-    const int i_total_valence)
+FLO_API cusp::coo_matrix<int, real, cusp::device_memory>
+cotangent_laplacian(const thrust::device_ptr<const real3> di_vertices,
+                    const thrust::device_ptr<const int3> di_faces,
+                    const thrust::device_ptr<const real> di_face_area,
+                    const int i_nverts,
+                    const int i_nfaces,
+                    const int i_total_valence)
 {
   using SparseMatrix = cusp::coo_matrix<int, real, cusp::device_memory>;
   SparseMatrix d_L(i_nverts, i_nverts, i_total_valence + i_nverts);
@@ -178,36 +191,41 @@ FLO_API cusp::coo_matrix<int, real, cusp::device_memory> cotangent_laplacian(
   thrust::device_vector<real> V(ntriplets);
 
   dim3 block_dim;
-  block_dim.x = 1024 / 12; 
+  block_dim.x = 1024 / 12;
   block_dim.y = 4;
   block_dim.z = 3;
   size_t nthreads_per_block = block_dim.x * block_dim.y * block_dim.z;
   size_t nblocks = ntriplets / nthreads_per_block + 1;
-  size_t shared_memory_size = sizeof(flo::real)*block_dim.x*7;
+  // face area | cot_alpha  =>  sizeof(real) * 3
+  // vertex positions       =>  sizeof(real3) * 3 ==  sizeof(real) * 9
+  // edge squared lengths   =>  sizeof(real) * 3
+  // === (3 + 9 + 3) * sizeof(real)
+  size_t shared_memory_size = sizeof(flo::real) * block_dim.x * 15;
 
+  d_build_triplets<<<nblocks, block_dim, shared_memory_size>>>(di_vertices,
+                                                               di_faces,
+                                                               di_face_area,
+                                                               i_nfaces,
+                                                               I.data(),
+                                                               J.data(),
+                                                               V.data());
+  cudaDeviceSynchronize();
 
-  d_build_triplets<<<nblocks, block_dim, shared_memory_size>>>(
-        di_vertices, 
-        di_faces, 
-        di_face_area, 
-        i_nfaces, 
-        I.data(), 
-        J.data(), 
-        V.data());
- 
   {
     using namespace thrust;
-    stable_sort_by_key(J.begin(), J.end(), make_zip_iterator(make_tuple(I.begin(), V.begin())));
-    stable_sort_by_key(I.begin(), I.end(), make_zip_iterator(make_tuple(J.begin(), V.begin())));
+    stable_sort_by_key(
+      J.begin(), J.end(), make_zip_iterator(make_tuple(I.begin(), V.begin())));
+    stable_sort_by_key(
+      I.begin(), I.end(), make_zip_iterator(make_tuple(J.begin(), V.begin())));
 
-    reduce_by_key(
-        make_zip_iterator(make_tuple(I.begin(), J.begin())),
-        make_zip_iterator(make_tuple(I.end(), J.end())),
-        V.begin(),
-        make_zip_iterator(make_tuple(d_L.row_indices.begin(), d_L.column_indices.begin())),
-        d_L.values.begin(),
-        equal_to<tuple<int,int>>(),
-        plus<float>());
+    reduce_by_key(make_zip_iterator(make_tuple(I.begin(), J.begin())),
+                  make_zip_iterator(make_tuple(I.end(), J.end())),
+                  V.begin(),
+                  make_zip_iterator(make_tuple(d_L.row_indices.begin(),
+                                               d_L.column_indices.begin())),
+                  d_L.values.begin(),
+                  equal_to<tuple<int, int>>(),
+                  plus<float>());
   }
 
   return d_L;
