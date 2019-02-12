b6ac27177a054e0a7f2931185deec7380341324c
Jack Diver <s4902673@bournemouth.ac.uk>
2019-02-12 14:04:51 +0000

    Fixed a bug in the matrix construction from triplets. Second sort should be stable to ensure all matching IJ keys become neighbours.

--- flo/src/flo/device/cotangent_laplacian.cu
+++ flo/src/flo/device/cotangent_laplacian.cu
@@ -190,6 +190,23 @@ void multi_sort_by_key(RandomAccessIterator1&& i_key_begin,
           0)...};
 }
 
+template <typename RandomAccessIterator1,
+          typename RandomAccessIterator2,
+          typename... RandomAccessIterator3>
+void multi_stable_sort_by_key(RandomAccessIterator1&& i_key_begin,
+                              RandomAccessIterator1&& i_key_end,
+                              RandomAccessIterator2&& i_new_key_begin,
+                              RandomAccessIterator3&&... i_data_begin)
+{
+  using expand = int[];
+  auto new_key_end = i_new_key_begin + (i_key_end - i_key_begin);
+  thrust::sequence(i_new_key_begin, new_key_end);
+  thrust::stable_sort_by_key(i_key_begin, i_key_end, i_new_key_begin);
+  expand{((void)thrust::gather(
+            i_new_key_begin, new_key_end, i_data_begin, i_data_begin),
+          0)...};
+}
+
 FLO_API cusp::coo_matrix<int, real, cusp::device_memory>
 cotangent_laplacian(const thrust::device_ptr<const real3> di_vertices,
                     const thrust::device_ptr<const int3> di_faces,
@@ -231,7 +248,7 @@ cotangent_laplacian(const thrust::device_ptr<const real3> di_vertices,
 
   thrust::device_vector<int> seq(ntriplets);
   multi_sort_by_key(J.begin(), J.end(), seq.begin(), I.begin(), V.begin());
-  multi_sort_by_key(I.begin(), I.end(), seq.begin(), J.begin(), V.begin());
+  multi_stable_sort_by_key(I.begin(), I.end(), seq.begin(), J.begin(), V.begin());
 
   //int num_entries =
   //  thrust::inner_product(coord_begin,
