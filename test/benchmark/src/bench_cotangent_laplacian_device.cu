6d17346f3c5fc8f5d7802abca0d13d0ad6fbc6e6
nitronoid <jackdiver@hotmail.co.uk>
2019-02-27 12:21:28 +0000

    Updated cotangent laplacian benchmark to work with new api and calculate diagonals.

--- test/benchmark/src/bench_cotangent_laplacian_device.cu
+++ test/benchmark/src/bench_cotangent_laplacian_device.cu
@@ -33,7 +33,7 @@
   }                                                                         \
   BENCHMARK(BM_NAME)
 
-#define DEVICE_BM_CLM(BM_NAME, FILE_NAME)                                   \
+#define DEVICE_BM_CLA(BM_NAME, FILE_NAME)                                   \
   static void BM_NAME(benchmark::State& state)                              \
   {                                                                         \
     auto surf = TestCache::get_mesh<TestCache::DEVICE>(FILE_NAME);          \
@@ -58,7 +58,7 @@
     SparseMatrix d_L(surf.n_vertices(),                                     \
                      surf.n_vertices(),                                     \
                      d_cumulative_valence.back() + surf.n_vertices());      \
-    thrust::fill(d_L.values.begin(), d_L.values.end(), 0);                  \
+    thrust::device_vector<int> d_diagonals(surf.n_vertices());              \
     for (auto _ : state)                                                    \
     {                                                                       \
       flo::device::cotangent_laplacian(surf.vertices.data(),                \
@@ -66,45 +66,13 @@
                                        d_area.data(),                       \
                                        d_cumulative_valence.data(),         \
                                        d_offsets.data(),                    \
+                                       surf.n_vertices(),                   \
                                        surf.n_faces(),                      \
+                                       d_cumulative_valence.back(),         \
+                                       d_diagonals.data(),                  \
                                        d_L.row_indices.data(),              \
                                        d_L.column_indices.data(),           \
                                        d_L.values.data());                  \
-    }                                                                       \
-  }                                                                         \
-  BENCHMARK(BM_NAME)
-
-#define DEVICE_BM_CLA(BM_NAME, FILE_NAME)                                   \
-  static void BM_NAME(benchmark::State& state)                              \
-  {                                                                         \
-    auto surf = TestCache::get_mesh<TestCache::DEVICE>(FILE_NAME);          \
-    auto d_area = flo::device::area(                                        \
-      surf.vertices.data(), surf.faces.data(), surf.faces.size());          \
-    thrust::device_vector<int> d_valence(surf.n_vertices());                \
-    thrust::device_vector<int> d_cumulative_valence(surf.n_vertices() + 1); \
-    auto d_adjacency =                                                      \
-      flo::device::vertex_vertex_adjacency(surf.faces.data(),               \
-                                           surf.n_faces(),                  \
-                                           surf.n_vertices(),               \
-                                           d_valence.data(),                \
-                                           d_cumulative_valence.data());    \
-                                                                            \
-    auto d_offsets =                                                        \
-      flo::device::adjacency_matrix_offset(surf.faces.data(),               \
-                                           d_adjacency.data(),              \
-                                           d_cumulative_valence.data(),     \
-                                           surf.n_faces());                 \
-    for (auto _ : state)                                                    \
-    {                                                                       \
-      benchmark::DoNotOptimize(                                             \
-        flo::device::cotangent_laplacian(surf.vertices.data(),              \
-                                         surf.faces.data(),                 \
-                                         d_area.data(),                     \
-                                         d_cumulative_valence.data(),       \
-                                         d_offsets.data(),                  \
-                                         surf.n_vertices(),                 \
-                                         surf.n_faces(),                    \
-                                         d_cumulative_valence.back()));     \
     }                                                                       \
   }                                                                         \
   BENCHMARK(BM_NAME)
@@ -114,12 +82,6 @@ DEVICE_BM_CLT(DEVICE_cotangent_laplacian_triplets_spot, "../models/spot.obj");
 DEVICE_BM_CLT(DEVICE_cotangent_laplacian_triplets_sphere_400,
               "../models/dense_sphere_400x400.obj");
 DEVICE_BM_CLT(DEVICE_cotangent_laplacian_triplets_sphere_1000,
-              "../models/dense_sphere_1000x1000.obj");
-DEVICE_BM_CLM(DEVICE_cotangent_laplacian_no_alloc_cube_1, "../models/cube.obj");
-DEVICE_BM_CLM(DEVICE_cotangent_laplacian_no_alloc_spot, "../models/spot.obj");
-DEVICE_BM_CLM(DEVICE_cotangent_laplacian_no_alloc_sphere_400,
-              "../models/dense_sphere_400x400.obj");
-DEVICE_BM_CLM(DEVICE_cotangent_laplacian_no_alloc_sphere_1000,
               "../models/dense_sphere_1000x1000.obj");
 DEVICE_BM_CLA(DEVICE_cotangent_laplacian_atomic_cube_1, "../models/cube.obj");
 DEVICE_BM_CLA(DEVICE_cotangent_laplacian_atomic_spot, "../models/spot.obj");
