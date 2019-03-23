55749df63837efcc79bdbcb9974e175eb2086aef
nitronoid <jackdiver@hotmail.co.uk>
2019-03-23 15:38:28 +0000

    Removed unused meshes from the cache

--- test/host_tests/src/main.cpp
+++ test/host_tests/src/main.cpp
@@ -8,8 +8,8 @@ int main(int argc, char** argv)
 {
   TestCache::get_mesh<TestCache::HOST>("cube.obj");
   TestCache::get_mesh<TestCache::HOST>("spot.obj");
-  TestCache::get_mesh<TestCache::HOST>("dense_sphere_400x400.obj");
-  TestCache::get_mesh<TestCache::HOST>("dense_sphere_1000x1000.obj");
+  //TestCache::get_mesh<TestCache::HOST>("dense_sphere_400x400.obj");
+  //TestCache::get_mesh<TestCache::HOST>("dense_sphere_1000x1000.obj");
   // TestCache::get_mesh<TestCache::HOST>("dense_sphere_1500x1500.obj");
   // TestCache::get_mesh<TestCache::HOST>("cube_1k.obj");
   ::testing::InitGoogleTest(&argc, argv);
