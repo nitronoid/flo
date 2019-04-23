f579532497ede92bae818e8fc397241d90cecf31
nitronoid <jackdiver@hotmail.co.uk>
2019-04-23 20:33:42 +0100

    Added a sample for exporting all calculated matrices to mtx file format.

--- samples/host/matrix_bake/src/main.cpp
+++ samples/host/matrix_bake/src/main.cpp
@@ -2,6 +2,8 @@
 #include <numeric>
 #include <unsupported/Eigen/SparseExtra>
 #include <igl/writeOBJ.h>
+#include <igl/vertex_triangle_adjacency.h>
+#include <igl/adjacency_list.h>
 #include "flo/host/flo_matrix_operation.hpp"
 #include "flo/host/load_mesh.hpp"
 #include "flo/host/vertex_normals.hpp"
@@ -45,6 +47,100 @@ matrix_offsets(const gsl::span<Eigen::Vector3i> i_faces,
 
   return offsets;
 }
+
+struct Adjacency
+{
+  std::vector<int> adjacency_keys;
+  std::vector<int> adjacency;
+  std::vector<int> valence;
+  std::vector<int> cumulative_valence;
+};
+
+template <typename T>
+std::vector<T> flatten(const std::vector<std::vector<T>>& array2d)
+{
+  // Get the total number of elements in the 2d vector
+  const int N = std::accumulate(
+    array2d.begin(), array2d.end(), 0, [](int lhs, const std::vector<T>& rhs) {
+      return lhs + rhs.size();
+    });
+  std::vector<T> array1d;
+  array1d.reserve(N);
+  for (const auto& row : array2d)
+  {
+    std::copy(row.begin(), row.end(), std::back_inserter(array1d));
+  }
+  return array1d;
+}
+
+Adjacency vertex_triangle_adjacency(const gsl::span<Eigen::Vector3i> i_faces)
+{
+  const auto F = flo::host::array_to_matrix(i_faces);
+  const int nverts = F.maxCoeff() + 1;
+  std::cout << "NV: " << nverts << '\n';
+  Adjacency ret;
+
+  Eigen::Matrix<int, Eigen::Dynamic, 1> VF;
+  Eigen::Matrix<int, Eigen::Dynamic, 1> NI;
+  igl::vertex_triangle_adjacency(F, nverts, VF, NI);
+  ret.adjacency = flo::host::matrix_to_array(VF);
+  ret.cumulative_valence = flo::host::matrix_to_array(NI);
+
+  ret.valence.reserve(nverts);
+  std::adjacent_difference(ret.cumulative_valence.begin() + 1,
+                           ret.cumulative_valence.end(),
+                           std::back_inserter(ret.valence));
+
+  ret.adjacency_keys.reserve(ret.cumulative_valence.back());
+  for (int i = 0; i < nverts; ++i)
+  {
+    const int v = ret.valence[i];
+    for (int j = 0; j < v; ++j)
+    {
+      ret.adjacency_keys.push_back(i);
+    }
+  }
+
+  return ret;
+}
+
+Adjacency vertex_vertex_adjacency(const gsl::span<Eigen::Vector3i> i_faces)
+{
+  const auto Fm = flo::host::array_to_matrix(i_faces);
+  Eigen::MatrixXi F = Fm;
+
+  const int nverts = F.maxCoeff() + 1;
+  std::cout << "NV: " << nverts << '\n';
+  Adjacency ret;
+
+  std::vector<std::vector<int>> A;
+  igl::adjacency_list(F, A, false);
+  ret.adjacency = flatten(A);
+
+  ret.valence.reserve(nverts);
+  std::transform(A.begin(),
+                 A.end(),
+                 std::back_inserter(ret.valence),
+                 [](const std::vector<int>& vec) -> int { return vec.size(); });
+
+  ret.cumulative_valence.reserve(nverts + 1);
+  ret.cumulative_valence.push_back(0);
+  std::partial_sum(ret.valence.begin(),
+                   ret.valence.end(),
+                   std::back_inserter(ret.cumulative_valence));
+
+  ret.adjacency_keys.reserve(ret.cumulative_valence.back());
+  for (int i = 0; i < nverts; ++i)
+  {
+    const int v = ret.valence[i];
+    for (int j = 0; j < v; ++j)
+    {
+      ret.adjacency_keys.push_back(i);
+    }
+  }
+
+  return ret;
+}
 }  // namespace
 
 int main()
@@ -55,6 +151,34 @@ int main()
   using namespace flo;
   using namespace flo::host;
   auto surf = load_mesh((name + ".obj").c_str());
+
+  auto vta = vertex_triangle_adjacency(surf.faces);
+  const auto VTAK = array_to_matrix(gsl::make_span(vta.adjacency_keys));
+  const auto VTA = array_to_matrix(gsl::make_span(vta.adjacency));
+  const auto VTV = array_to_matrix(gsl::make_span(vta.valence));
+  const auto VTCV = array_to_matrix(gsl::make_span(vta.cumulative_valence));
+  Eigen::saveMarketVector(
+    VTAK, matrix_prefix + "vertex_triangle_adjacency/adjacency_keys.mtx");
+  Eigen::saveMarketVector(
+    VTA, matrix_prefix + "vertex_triangle_adjacency/adjacency.mtx");
+  Eigen::saveMarketVector(
+    VTV, matrix_prefix + "vertex_triangle_adjacency/valence.mtx");
+  Eigen::saveMarketVector(
+    VTCV, matrix_prefix + "vertex_triangle_adjacency/cumulative_valence.mtx");
+
+  auto vva = vertex_vertex_adjacency(surf.faces);
+  const auto VVAK = array_to_matrix(gsl::make_span(vva.adjacency_keys));
+  const auto VVA = array_to_matrix(gsl::make_span(vva.adjacency));
+  const auto VVV = array_to_matrix(gsl::make_span(vva.valence));
+  const auto VVCV = array_to_matrix(gsl::make_span(vva.cumulative_valence));
+  Eigen::saveMarketVector(
+    VVAK, matrix_prefix + "vertex_vertex_adjacency/adjacency_keys.mtx");
+  Eigen::saveMarketVector(
+    VVA, matrix_prefix + "vertex_vertex_adjacency/adjacency.mtx");
+  Eigen::saveMarketVector(
+    VVV, matrix_prefix + "vertex_vertex_adjacency/valence.mtx");
+  Eigen::saveMarketVector(
+    VVCV, matrix_prefix + "vertex_vertex_adjacency/cumulative_valence.mtx");
 
   // Calculate smooth vertex normals
   const auto normals = vertex_normals(surf.vertices, surf.faces);
@@ -89,16 +213,10 @@ int main()
   const auto FA = array_to_matrix(gsl::make_span(face_area));
   Eigen::saveMarketVector(FA, matrix_prefix + "face_area/face_area.mtx");
 
-  // Calculate the valence of every vertex to allocate sparse matrices
-  const auto vertex_valence = valence(surf.faces);
-  const auto VV = array_to_matrix(gsl::make_span(vertex_valence));
-  Eigen::saveMarketVector(
-    VV, matrix_prefix + "vertex_vertex_adjacency/valence.mtx");
-
   std::vector<real> rho(surf.n_vertices(), 3.f);
   // Calculate the intrinsic dirac operator matrix
   const auto D =
-    intrinsic_dirac(surf.vertices, surf.faces, vertex_valence, face_area, rho);
+    intrinsic_dirac(surf.vertices, surf.faces, vva.valence, face_area, rho);
   Eigen::saveMarket(D, matrix_prefix + "intrinsic_dirac/intrinsic_dirac.mtx");
 
   // Calculate the scaling and rotation for our spin transformation
