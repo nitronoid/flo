928395d62e00b1763161b8c44a511ac401f3c91c
Jack Diver <s4902673@bournemouth.ac.uk>
2019-04-30 00:38:10 +0100

    Added a benchmark for the host side quaternion matrix expansion

---
+++ test/benchmark/host_benchmark/src/bench_quaternion_matrix.cpp
@@ -0,0 +1,35 @@
+#include <benchmark/benchmark.h>
+#include "test_common.h"
+#include "flo/host/flo_matrix_operation.hpp"
+
+namespace
+{
+static void bench_impl(std::string name, benchmark::State& state)
+{
+  const std::string mp = "../../matrices/" + name;
+  auto surf = TestCache::get_mesh<TestCache::HOST>(name + ".obj");
+  auto L = read_sparse_matrix<flo::real>(
+    mp + "/cotangent_laplacian/cotangent_laplacian.mtx");
+
+  Eigen::Matrix<flo::real, Eigen::Dynamic, 4> E;
+  for (auto _ : state)
+  {
+    benchmark::DoNotOptimize(flo::host::to_real_quaternion_matrix(L));
+  }
+}
+}  // namespace
+
+#define FLO_QUATERNION_MATRIX_HOST_BENCHMARK(NAME)                   \
+  static void HOST_quaternion_matrix_##NAME(benchmark::State& state) \
+  {                                                                  \
+    bench_impl(#NAME, state);                                        \
+  }                                                                  \
+  BENCHMARK(HOST_quaternion_matrix_##NAME);
+
+FLO_QUATERNION_MATRIX_HOST_BENCHMARK(cube)
+FLO_QUATERNION_MATRIX_HOST_BENCHMARK(spot)
+FLO_QUATERNION_MATRIX_HOST_BENCHMARK(bunny)
+
+#undef FLO_QUATERNION_MATRIX_HOST_BENCHMARK
+
+
